ee27b5164ac4ba45509ad3da29a010910aacf613
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-10-01 11:38:48 +0000

    show作成開始

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
   def mypage
     @mypage = User.find(current_user.id)
-    
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = 
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
