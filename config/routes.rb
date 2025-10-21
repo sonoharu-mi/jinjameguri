9efc2652a4260d1a0c5950bf69fa264395d619c6
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-10-21 14:14:00 +0000

    comment_作成開始

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
   end
+
   root to: "homes#top"
   devise_for :users
   get "/homes/about" => "homes#about", as: "about"
   get '/mypage' => "users#mypage", as: "mypage"
+  resources :posts do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:mypage, :show, :edit, :update, :destroy]
-  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
