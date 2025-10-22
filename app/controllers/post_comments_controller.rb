c26da66fa2a8247f3b488248654ea8b35c8b5fc9
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-10-22 11:39:10 +0000

    comment_create完成

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,8 +1,8 @@
 class PostCommentsController < ApplicationController
   def create
-    post = PostComment.find(params[:post_id])
+    post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post_id
+    comment.post_id = post.id
     comment.save
     redirect_to post_path(post)
   end
