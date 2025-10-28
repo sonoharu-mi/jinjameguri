392d942406a8067cdc49421f4aee1e895788d284
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-10-28 14:23:40 +0000

    search_検索結果

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,10 @@
 class SearchesController < ApplicationController
   def search
     @model = params[:model]
-    @content = params[:content]
+    @content = params[:shirine_name]
     @method = params[:method]
+
+    @model == "post"
+    @records = Post.search_for(@content, @method)
   end
 end
