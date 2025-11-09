af47c30f235617af77b283d330e6886142c8f754
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-11-09 10:35:31 +0000

    group_index作成中

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,23 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-    
-  end
   
   def create
+    @group = current_user.owned_groups.build(group_params)
+    @group.users << current_user
+
+    if @group.save
+      redirect_to @group, notice: "グループを作成しました"
+    else
+      @group = Group.all
+      flash.now[:error] = "グループの作成に失敗しました"
+      render :index
+    end
   end
 
   def index
     @groups = Group.all
-    @group = Groupe.new
+    @group = Group.new
   end
 
   def show
@@ -22,5 +29,9 @@ class GroupsController < ApplicationController
   def destroy
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, :introduction)
+  end
 
 end
