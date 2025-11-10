689dd819ffc2f31c7697169b0cf8e22a379cfff2
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-11-10 12:13:21 +0000

    group_申請作成開始

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_11_07_120108) do
+ActiveRecord::Schema.define(version: 2025_11_10_112507) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2025_11_07_120108) do
     t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["group_id"], name: "index_group_users_on_group_id"
     t.index ["user_id"], name: "index_group_users_on_user_id"
   end
