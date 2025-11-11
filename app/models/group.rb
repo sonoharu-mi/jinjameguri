a50701f29a0d5a6e86c1f159bc6cfe3c3d43347a
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-11-11 14:18:58 +0000

    group_image追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,14 @@ class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
-
+  has_one_attached :image
   validates :name, presence: true
-  
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_group_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-iamge.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
