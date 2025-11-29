5b14c05286559d6289c261ba37f5e95496693104
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-11-29 15:13:11 +0000

    calendar_form作成開始

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,7 +3,16 @@ class CalendarsController < ApplicationController
     @events = Calendar.all
     respond_to do |format|
       format.html
-      format.json
+      format.json {
+        render json: @events.map { |e|
+          {
+          id: e.id,
+          event: "#{e.prefecture}#{e.city}：#{e.event}",
+          start: e.start_date,
+          end: e.end_date
+          }
+        }
+      }
     end
   end
 end
