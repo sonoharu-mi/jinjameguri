4323fe0331994f47d0a54f316fc416e81975ff66
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2026-01-08 13:51:38 +0000

    admin_ページネーション他修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc).page(params[:page])
   end
 
   def destroy
