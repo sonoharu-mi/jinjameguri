aba4002287857c0254f1e7359f2a07fd89d687d8
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2026-01-22 13:23:09 +0000

    jquery_topレイアウト

--- app/javascript/packs/jquery_app.js
+++ app/javascript/packs/jquery_app.js
@@ -1,3 +1,22 @@
 $(function() {
-  $('.box1').slideDown();
+  const box = $('.box1');
+  const text1 = $('.text1');
+  const text2 = $('.text2');
+  const text3 = $('.text3');
+
+
+  text1.slideDown(2000, function() {
+    text2.slideDown(2000, function() {
+      text3.slideDown(2000);
+    });
+  });
+    
+  box.hover(
+    function() {
+      box.addClass('box1-ext');
+    },
+    function() {
+      box.removeClass('box1-ext');
+    }
+  );
 });
